8b75e5ee73af3e86a8a220e43d7d18c3c5407da7
borchr27 <30349201+borchr27@users.noreply.github.com>
2019-12-28 17:24:23 -0500

    First commit

---
+++ DinerMax3000Console/MenuItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinerMax3000Console
+{
+    public class MenuItem
+    {
+        public string Title;
+        public string Description;
+        public double Price;
+    }
+}
---
+++ DinerMax3000Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DinerMax3000Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu summerMenu = new Menu();
+            summerMenu.Name = "Summer Menu";
+            summerMenu.AddMenuItem("Salmon", "Fresh Norwegian Salmon with butter.", 25.50);
+            summerMenu.AddMenuItem("Tacos", "All Norwegians eat tacos on Fridays", 10);
+            
+        }
+    }
+}
