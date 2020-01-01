837348235b8025922930523beb48c024e9be4d07
borchr27 <30349201+borchr27@users.noreply.github.com>
2020-01-01 12:23:35 -0500

    Updated database functionality

--- DinerMax3000BusinessLayer/Menu.cs
+++ DinerMax3000BusinessLayer/Menu.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DinerMax3000.Business.dsDinerMax3000TableAdapters;
 
 namespace DinerMax3000.Business
 {
@@ -9,6 +10,27 @@ namespace DinerMax3000.Business
         public Menu()
         {
             items = new List<MenuItem>();
+        }
+
+        public static List<Menu> GetAllMenus() 
+        {
+            MenuTableAdapter taMenu = new MenuTableAdapter();
+            MenuItemTableAdapter taMenuItem = new MenuItemTableAdapter;
+            var dtMenu = taMenu.GetData();
+            List<Menu> allMenus = new List<Menu>();
+            foreach(dsDinerMax3000.MenuRow menuRow in dtMenu.Rows) 
+            {
+                Menu currentMenu = new Menu();
+                currentMenu.Name = menuRow.Name;
+                allMenus.Add(currentMenu);
+
+                var dtMenuItems = taMenuItem.GetMenuItemsByMenuId(menuRow.Id);
+                foreach(dsDinerMax3000.MenuItemRow menuItemRow in dtMenuItems.Rows) 
+                {
+                    currentMenu.AddMenuItem(menuItemRow.Name, menuItemRow.Description, menuItemRow.Price);
+                }
+            }
+            return allMenus;
         }
 
         public void AddMenuItem(string Title, string Description, double Price)
--- DinerMax3000Console/Program.cs
+++ DinerMax3000Console/Program.cs
@@ -8,38 +8,17 @@ namespace DinerMax3000Console
     {
         static void Main(string[] args)
         {
-            FoodMenu summerMenu = new FoodMenu();
-            summerMenu.Name = "Summer Menu";
-            summerMenu.AddMenuItem("Salmon", "Fresh Norwegian Salmon with butter.", 25);
-            summerMenu.AddMenuItem("Tacos", "All Norwegians eat tacos on Fridays", 10);
-            summerMenu.HospitalDirections = "Go right on 5th street and its just down a ways, ask for Dr. Gein";
-
-            DrinkMenu outsideDrinks = new DrinkMenu();
-            outsideDrinks.Disclaimer = "Outside drinks are absolutely allowed, please remeber to code responsibly";
-            outsideDrinks.AddMenuItem("Screwdriver", "Gets you turnt up real quick", 15);
-            outsideDrinks.AddMenuItem("Cuban Virgin Libre","Always a good time, makes sure the boys have fun but not to much fun",10);
+            List<Menu> menusFromDatabase = Menu.GetAllMenus();
 
             Order HungryGuestOrder = new Order();
 
-            for (int x = 0; x <= summerMenu.items.Count - 1; x++) 
+            foreach(Menu currentMenu in menusFromDatabase)
             {
-                MenuItem currentItem = summerMenu.items[x];
-                HungryGuestOrder.items.Add(currentItem);
+                foreach (MenuItem currentitem in currentMenu.items)
+                {
+                    HungryGuestOrder.items.Add(currentitem);
+                }
             }
-
-            foreach (MenuItem currentitem in outsideDrinks.items) 
-            {
-                HungryGuestOrder.items.Add(currentitem);
-            }
-            try
-            {
-                outsideDrinks.AddMenuItem("Extra V", "Always a good time, makes sure the boys have fun but not to much fun", 0);
-            }
-            catch (Exception thrownException)
-            {
-                Console.WriteLine(thrownException.Message);
-            }
-
             Console.WriteLine("The total is: " + "$" + HungryGuestOrder.Total);
         }
     }
